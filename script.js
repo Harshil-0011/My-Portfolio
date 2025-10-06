56dcf49f869383c70a8991074a877b72a695eec8
Harshil Gorasiya <109842861+Harshil-0011@users.noreply.github.com>
2025-10-06 11:09:26 +0200

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ toggleBtn.addEventListener("click", () => {
   const currentTheme = html.getAttribute("data-theme");
   html.setAttribute("data-theme", currentTheme === "light" ? "dark" : "light");
 });
+
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('fade-in');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
